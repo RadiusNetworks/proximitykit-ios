ecd467da4d4d4e4e171ac1c89d463e65b68a74f6
Christopher Sexton <github@codeography.com>
2015-01-20 09:16:22 -0500

    Updated run time check for NSLocationWhenInUseUsageDescription
    Fixes #4

--- ProximityKit.framework/Headers/ProximityKit.h
+++ ProximityKit.framework/Headers/ProximityKit.h
@@ -6,3 +6,13 @@
 
 #import <Foundation/Foundation.h>
 #import <ProximityKit/RPKManager.h>
+#import <ProximityKit/RPKBeacon.h>
+#import <ProximityKit/RPKBeaconRegion.h>
+#import <ProximityKit/RPKKit.h>
+#import <ProximityKit/RPKLogger.h>
+#import <ProximityKit/RPKManagerDelegate.h>
+#import <ProximityKit/RPKMap.h>
+#import <ProximityKit/RPKRegion.h>
+#import <ProximityKit/RPKManager.h>
+#import <ProximityKit/RPKCircle.h>
+#import <ProximityKit/RPKAnalyticsEvent.h>
