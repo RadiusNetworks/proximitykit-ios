32db4dfbce359af1f1ba2d2b25470cba774f2657
Christopher Sexton <github@codeography.com>
2015-12-02 16:40:07 -0500

    Version 1.1.3

--- ProximityKit.framework/Headers/RPKManager.h
+++ ProximityKit.framework/Headers/RPKManager.h
@@ -34,6 +34,7 @@ FOUNDATION_EXPORT NSString *const RPKManagerDidEnterRegionNotification;
 FOUNDATION_EXPORT NSString *const RPKManagerDidExitRegionNotification;
 FOUNDATION_EXPORT NSString *const RPKManagerDidRangeBeaconsInRegionNotification;
 FOUNDATION_EXPORT NSString *const RPKManagerDidTriggerAnalyticsEvent;
+FOUNDATION_EXPORT NSString *const RPKManagerDidSyncEvent;
 
 // Notification user data keys.
 FOUNDATION_EXPORT NSString *const RPKManagerNotificationEventKey;
@@ -42,6 +43,7 @@ FOUNDATION_EXPORT NSString *const RPKManagerNotificationRegionStateKey;
 FOUNDATION_EXPORT NSString *const RPKManagerNotificationBeaconsKey;
 FOUNDATION_EXPORT NSString *const RPKManagerNotificationAnalyticsEventDetailsKey;
 FOUNDATION_EXPORT NSString *const RPKManagerNotificationAnalyticsEventTypeKey;
+FOUNDATION_EXPORT NSString *const RPKManagerNotificationKitKey;
 
 
 /** RPKManager
