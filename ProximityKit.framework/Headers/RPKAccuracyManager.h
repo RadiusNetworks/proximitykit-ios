2cb110e66721da1b0e1439234dba7268998c8beb
Scott Newman <snewman18@gmail.com>
2016-02-10 12:56:38 -0700

    Version 1.2.0

---
+++ ProximityKit.framework/Headers/RPKAccuracyManager.h
@@ -0,0 +1,26 @@
+//
+//  RPKAccuracyManager.h
+//  ProximityKit
+//
+//  Created by Scott Newman on 1/19/16.
+//  Copyright © 2016 Radius Networks. All rights reserved.
+//
+
+@import UIKit;
+#import <Foundation/Foundation.h>
+
+@class RPKBeacon;
+@class RPKBeaconRegion;
+
+@protocol RPKAccuracyManagerDelegate <NSObject>
+@required
+- (void)closestBeaconDidChange:(RPKBeacon *)newClosestBeacon forRegion:(RPKBeaconRegion *)region;
+@end
+
+@interface RPKAccuracyManager : NSObject
+
+@property (nonatomic, assign) id<RPKAccuracyManagerDelegate> delegate;
+
+- (void)didRangeBeacons:(NSArray *)pkBeacons inRegion:(RPKBeaconRegion *)region;
+
+@end
